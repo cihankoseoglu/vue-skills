693053eef0471c14ee806f3d879f0c5532f691f0
Cihan <work@Cihans-MacBook-Pro-4.local>
2018-08-02 21:23:42 +0300

    Add Skills.vue, animate.css, vee-validate

---
+++ src/components/Skills.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+      <form @submit.prevent="addSkill">
+        <input type="text" placeholder="Enter a skill you have" v-model="skill" v-validate="'min:5'" name="skill">
+        <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+          <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+        </transition>
+      </form>
+      <ul>
+        <li v-for="(data, index) in skills" :key='index'>{{ data.skill }}</li>
+      </ul>
+      <p>These are the skills that you possess.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      skill: '',
+      skills: [
+        { 'skill': 'Vue.js' },
+        { 'skill': 'Front end' },
+      ]
+    }
+  },
+  methods: {
+    addSkill() {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.skills.push({skill: this.skill})
+          this.skill = ''
+        }
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./Skills.css" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VeeValidate from 'vee-validate';
+import 'animate.css';
 
+Vue.use(VeeValidate);
 Vue.config.productionTip = false
 
 new Vue({
